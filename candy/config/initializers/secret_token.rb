909612d226f753fc47e183020eb6c234d7bb3737
Marcos Martins <marcos.martins2307@gmail.com>
2014-02-04 09:22:27 -0700

    First commit

---
+++ candy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Candy::Application.config.secret_token = 'fdd6e0dc596233916b8d16ac0f1bdac7772e559034ae73ee475db02a0692860cacebd02189a8312bb96bb81023245c2f3a8c426846fa0f79b74897696e6c7030'
