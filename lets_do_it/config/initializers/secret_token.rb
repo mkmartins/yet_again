74e6cceb2fc56e9db87bc25032ba5c135458bae8
Marcos Martins <marcos.martins2307@gmail.com>
2014-02-04 08:45:46 -0700

    First commit

---
+++ lets_do_it/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LetsDoIt::Application.config.secret_token = '385cba1b80a5e177a3ec25267e279cccf4f6cde50631c06d457da7795df8415c29b1289427caff5452f0c0a9602e3528ffcac694566225e7028f7ea94298c6dc'
